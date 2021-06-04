a59454870daaa7ef2b9c7c1c336b8a146aed110e
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2021-06-04 16:48:31 -0300

    Documentar Categorias com swagger

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,13 +1,17 @@
 import express, { Request, NextFunction, Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
 import 'express-async-errors'
 
 import AppError from './errors/AppError'
 import router from './routes'
+import swaggerFile from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
-app.use(router)
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/api', router)
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
